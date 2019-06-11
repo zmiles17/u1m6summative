1aae8d15aaef7a1994687ebfad58220f716c495b
MoNsTeR <zgm_striker17@hotmail.com>
2019-06-11 13:51:51 -0500

    Initial Commit

---
+++ src/main/java/com/example/u1m6summative/U1m6summativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.u1m6summative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1m6summativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1m6summativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/u1m6summative/U1m6summativeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.u1m6summative;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1m6summativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
