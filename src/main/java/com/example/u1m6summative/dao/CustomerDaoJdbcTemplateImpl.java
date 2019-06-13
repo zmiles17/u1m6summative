d70d73a2495dbf23bf8555f69738782419aa67d7
MoNsTeR <zgm_striker17@hotmail.com>
2019-06-13 13:39:34 -0500

    customer tests

--- src/main/java/com/example/u1m6summative/dao/CustomerDaoJdbcTemplateImpl.java
+++ src/main/java/com/example/u1m6summative/dao/CustomerDaoJdbcTemplateImpl.java
@@ -1,33 +1,86 @@
 package com.example.u1m6summative.dao;
 
 import com.example.u1m6summative.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 @Repository
 public class CustomerDaoJdbcTemplateImpl implements CustomerDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    private static final String INSERT_CUSTOMER_SQL =
+            "INSERT INTO customer (first_name, last_name, email, company, phone) VALUES (?, ?, ?, ?, ?)";
+
+    private static final String SELECT_CUSTOMER_SQL =
+            "SELECT * FROM customer WHERE customer_id = ?";
+
+    private static final String SELECT_ALL_CUSTOMERS_SQL =
+            "SELECT * FROM customer";
+
+    private static final String DELETE_CUSTOMER_SQL =
+            "DELETE FROM customer WHERE customer_id = ?";
+
+    private static final String UPDATE_CUSTOMER_SQL =
+            "UPDATE customer set first_name = ?, last_name = ?, email = ?, company = ?, phone = ? where customer_id = ?";
+
+    @Autowired
+    public CustomerDaoJdbcTemplateImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Customer addCustomer(Customer customer) {
-        return null;
+        jdbcTemplate.update(INSERT_CUSTOMER_SQL,
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getCompany(),
+                customer.getPhone());
+        int id = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
+        customer.setCustomerId(id);
+        return customer;
     }
 
     @Override
     public List<Customer> getAllCustomer() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_CUSTOMERS_SQL, this::mapRowToCustomer);
     }
 
     @Override
     public Customer getCustomer(int id) {
-        return null;
+        try {
+            return jdbcTemplate.queryForObject(SELECT_CUSTOMER_SQL, this::mapRowToCustomer, id);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public Customer updateCustomer(Customer customer) {
-        return null;
+        jdbcTemplate.update(UPDATE_CUSTOMER_SQL, customer.getFirstName(), customer.getLastName(), customer.getEmail(), customer.getCompany(),customer.getPhone(), customer.getCustomerId());
+        return jdbcTemplate.queryForObject(SELECT_CUSTOMER_SQL, this::mapRowToCustomer, customer.getCustomerId());
     }
 
     @Override
     public void deleteCustomer(int id) {
+        jdbcTemplate.update(DELETE_CUSTOMER_SQL, id);
+    }
 
+    private Customer mapRowToCustomer(ResultSet rs, int rowNum) throws SQLException {
+        Customer customer = new Customer();
+        customer.setCustomerId(rs.getInt("customer_id"));
+        customer.setFirstName(rs.getString("first_name"));
+        customer.setLastName(rs.getString("last_name"));
+        customer.setEmail(rs.getString("email"));
+        customer.setCompany(rs.getString("company"));
+        customer.setPhone(rs.getString("phone"));
+
+        return customer;
     }
 }
--- src/main/java/com/example/u1m6summative/service/RentalStoreService.java
+++ src/main/java/com/example/u1m6summative/service/RentalStoreService.java
@@ -4,8 +4,14 @@ import com.example.u1m6summative.dao.CustomerDao;
 import com.example.u1m6summative.dao.InvoiceDao;
 import com.example.u1m6summative.dao.InvoiceItemDao;
 import com.example.u1m6summative.dao.ItemDao;
+import com.example.u1m6summative.model.Address;
+import com.example.u1m6summative.model.Customer;
+import com.example.u1m6summative.viewmodel.RentalStoreViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Component
 public class RentalStoreService {
@@ -22,4 +28,35 @@ public class RentalStoreService {
         this.itemDao = itemDao;
         this.invoiceItemDao = invoiceItemDao;
     }
+
+    @Transactional
+    public RentalStoreViewModel saveStore(RentalStoreViewModel viewModel) {
+        return null;
+
+    }
+
+    public Customer findCustomer(int id) {
+        return customerDao.getCustomer(id);
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return customerDao.addCustomer(customer);
+    }
+
+    public List<Customer> findAllCustomers() {
+        return customerDao.getAllCustomer();
+    }
+
+    public Customer updateCustomer(Customer customer) {
+        return customerDao.updateCustomer(customer);
+    }
+
+    public void deleteCustomer(int id) {
+        customerDao.deleteCustomer(id);
+    }
+
+    private RentalStoreViewModel buildStoreViewModel(Address address) {
+        return null;
+    }
+
 }
